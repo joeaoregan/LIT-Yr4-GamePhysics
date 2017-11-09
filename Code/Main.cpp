08141848cc542222c84b317d0000411fda0d18f5
Joe O'Regan <k00203642@student.lit.ie>
2017-11-09 03:48:15 +0000

    09/11/2017 Chapter 1.2

--- Code/Main.cpp
+++ Code/Main.cpp
@@ -8,6 +8,11 @@
 	Scene using Bullet and OpenGL
 */
 
-int main() {
-	return 0;
+#include "BulletOpenGLApplication.h"
+#include "FreeGLUTCallbacks.h"
+
+int main(int argc, char** argv) {
+	BulletOpenGLApplication demo;
+
+	return glutmain(argc, argv, 1024, 768, "Introduction to Game Physics with Bullet Physics and OpenGL", &demo);
 }
